a33bf8b52a479385d98ef1363b95d169bd9ca928
Edvan <edvanmmelo@gmail.com>
2019-01-07 13:58:12 -0300

    Conclusao do frontend sem redux

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -17,6 +17,7 @@ export default class Todo extends Component {
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
 
         this.refresh()
     }
@@ -58,6 +59,10 @@ export default class Todo extends Component {
             .then(resp => this.refresh(this.state.description))
     }
 
+    handleClear() {
+        this.refresh()
+    }
+
     render(){
         return (
             <div>
@@ -66,6 +71,7 @@ export default class Todo extends Component {
                 handleChange={this.handleChange}
                 handleAdd={this.handleAdd}
                 handleSearch={this.handleSearch}
+                handleClear={this.handleClear}
                 />
                 <TodoList list={this.state.list}
                 handleMarkAsDone={this.handleMarkAsDone}
