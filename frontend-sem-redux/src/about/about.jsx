26761261c177b9bd9fedf7aa73229f1285423783
Edvan <edvanmmelo@gmail.com>
2019-01-07 14:03:07 -0300

    clonada a pasta para o projeto sem redux, para reutilizacao dos fontes utilizando o redux

---
+++ frontend-sem-redux/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+
+export default props => (
+    <div>
+       <PageHeader name='Sobre' small='app'/>
+
+       <h2>Nossa História</h2>
+       <p>kasjdik dsifjsdif dsifj dsijf disof j</p>
+    </div>
+)
---
+++ frontend-sem-redux/src/template/iconButton.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import If from '../template/if'
+
+/**
+ * <button id='description' className='btn btn-primary'>
+                <i className='fa fa-plus'></i>
+            </button>
+ * 
+ */
+
+ /*
+export default props => {
+    if(props.hide){
+        return null
+    } else {
+        return (
+            <button className={'btn btn-'+ props.style }
+                onClick={props.onClick}>
+                <i className={'fa fa-'+ props.icon}></i>
+            </button>
+        )
+    }
+}*/
+
+export default props => (
+    <If test={!props.hide}>
+        <button className={'btn btn-'+ props.style} 
+            onClick={props.onClick}>
+            <i className={'fa fa-'+ props.icon}></i>
+        </button>
+    </If>
+)
